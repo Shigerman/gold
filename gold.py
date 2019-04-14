83685d2d1866733ce20e55b43510a932cad97c37
Larissa Shigerman <thelarisa81@gmail.com>
2019-04-14 12:22:35 +0300

    Finally the script works

--- gold.py
+++ gold.py
@@ -1,12 +1,12 @@
-# Взять текущую стоимость 10 гр. золотого слитка с сайта Сбербанка и сравнить её со стоимостью покупки
+# Get the current price of 10-gram gold bar from the bank site and compare it with the purchase price
 import requests
 from bs4 import BeautifulSoup as bs
 import openpyxl
 import xlrd
 
-# Зайти на нужную страницу Сбербанка
+# Open up the necessary page of the bank's site
 def get_html(url):
-# Подстраховаться от неработающего сервиса
+# Secure from the non-operating site
     try:
         result = requests.get(url)
         result.raise_for_status()
@@ -15,32 +15,25 @@ def get_html(url):
         print("сетевая ошибка")
         return False
 
-# Зайти на страницу Сбербанка с архивом курсов драгоценных металлов
 html = get_html("http://data.sberbank.ru/moscow/ru/quotes/archivoms/?base=beta")
 
-# Open the first file in the list
+# Open the first file in the files list
 if html:
     soup = bs(html, "html.parser")
-    print(type(soup))
     raw_script = soup.find("div", {'class': "layout-column"}).find('script').contents[0]
     raw_script = raw_script.split('url: "')[1].split('"')[0]
     link = "http://sberbank.ru" + raw_script
     raw_file = requests.get(link, allow_redirects=True)
     open('gold.xls', 'wb').write(raw_file.content)
 
+# Retrieve value from the first sheet and the necessary cell
+    book = xlrd.open_workbook("gold.xls", encoding_override="cp1252")
+    sheet = book.sheet_by_index(0)
+    new_price = int(sheet.cell_value(11, 3))
 
-# Retrieve value from D12 cell
-book = xlrd.open_workbook("gold.xls", encoding_override="cp1252")
-new_price = int(book['D12'].value) 
-print(new_price)
-
-#Вычесть из этой суммы сумму покупки 31341
-old_price = 31341
-price_difference = new_price - old_price
-
-#Выразить разницу в процентах
-percentage = (price_difference*100)/price_old
-print(f"{price_difference} руб., {percentage}%")
+# Substract the new price from the purchase price and express it as percent
+    percentage = ((new_price - 31341)*100)/31341
+    print(f"{new_price} - 31341 = {int(new_price - 31341)} rub., {int(percentage)}%")
 
 # Matplotlib
 # Database
