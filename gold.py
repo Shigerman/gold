015b1e40314933f7e05e085d3dc850d2f6a5ad9f
Larisa Shigareva <thelarisa81@gmail.com>
2020-11-07 21:26:49 +0300

    Refactor to make the code testable

--- gold.py
+++ gold.py
@@ -8,41 +8,85 @@ import textwrap
 import xlrd
 
 
+def get_current_date():
+    current_date = date.today() # 2020-03-06
+    return current_date.strftime('%d.%m.%Y')
+
+
+def get_current_month(current_date: str):
+    return int(current_date[3:5]) - 1 # months start from 0
+
+
+def get_current_year(current_date: str):
+    return int(current_date[6:10])
+
+
+def compile_bank_url(current_month, current_year):
+    url_start = "https://www.sberbank.ru/proxy/services/dict-services/"\
+                "document/list?groupCode=279&regionCode=77&month="
+    return url_start + str(current_month) + '&year=' + str(current_year)
+
+
+def request_price_file_list(current_month, current_year):
+    url = compile_bank_url(current_month, current_year)
+    try:
+        result = requests.get(url)
+        return result.content
+    except(requests.RequestException, ValueError):
+        print("Network error")
+        return False
+
+
+def download_gold_bar_prices(price_file_list):
+    most_recent_file_available = json.loads(price_file_list)[-1]
+    url_part_of_most_recent_file = most_recent_file_available['fileUrl']
+    prices_file_url = 'http://sberbank.ru' + url_part_of_most_recent_file
+    file_with_prices = requests.get(prices_file_url, allow_redirects=True)
+    return file_with_prices.content
+
+
+def gold_bar_price_from_xls(xls_file_name):
+    book = xlrd.open_workbook(xls_file_name, encoding_override="cp1252")
+    sheet = book.sheet_by_index(0)
+    return int(sheet.cell_value(11, 3))
+
+
+def count_price_diff_as_percent(new_price, OLD_PRICE):
+    return int(((new_price - OLD_PRICE) * 100) / OLD_PRICE)
+
+
+def gold_bar_price_from_xls(xls_file_name):
+    book = xlrd.open_workbook(xls_file_name, encoding_override="cp1252")
+    sheet = book.sheet_by_index(0)
+    return int(sheet.cell_value(11, 3))
+
+def count_price_diff_as_percent(new_price, OLD_PRICE):
+    return int(((new_price - OLD_PRICE) * 100) / OLD_PRICE)
+
+
+def save_new_price_to_make_a_graph():
+    date_list = []
+    with open("gold.csv", "r", encoding="cp1251") as file:
+        reader = csv.DictReader(file, delimiter=",")
+        for line in reader:
+            date_list.append(line['date'])
+
+    if date_list[-1] != current_date:
+        data = {'date': current_date,
+                'old_price': OLD_PRICE,
+                'new_price': new_price,
+                'percentage': price_diff_as_percent}
+        with open("gold.csv", "a", encoding="cp1251", newline="") as f:
+            fieldnames = ["date", "old_price", "new_price", "percentage"]
+            writer = csv.DictWriter(f, fieldnames, delimiter=",")
+            writer.writerow(data)
+
+
 def main():
-    def get_current_date_as_string():
-        current_date = date.today()
-        return current_date.strftime('%d.%m.%Y')
 
-
-    def get_current_month():
-        return int(current_date_str[3:5]) - 1 # months start from 0
-
-
-    def get_current_year():
-        return int(current_date_str[6:10])
-
-
-    current_date_str = get_current_date_as_string()
-    current_month = get_current_month()
-    current_year = get_current_year()
-
-
-    def compile_bank_url(current_month, current_year):
-        url_start = "https://www.sberbank.ru/proxy/services/dict-services/"\
-                    "document/list?groupCode=279&regionCode=77&month="
-        return url_start + str(current_month) + '&year=' + str(current_year)
-
-
-    def request_price_file_list(current_month, current_year):
-        url = compile_bank_url(current_month, current_year)
-        try:
-            result = requests.get(url)
-            return result.content
-        except(requests.RequestException, ValueError):
-            print("Network error")
-            return False
-
-
+    current_date = get_current_date()
+    current_month = get_current_month(current_date)
+    current_year = get_current_year(current_date)
     price_file_list = request_price_file_list(current_month, current_year)
 
     min_contents_size = 3 # '[]' for empty response
@@ -53,15 +97,6 @@ def main():
             if len(price_file_list) > min_contents_size:
                 break
 
-
-    def download_gold_bar_prices(price_file_list):
-        most_recent_file_available = json.loads(price_file_list)[-1]
-        url_part_of_most_recent_file = most_recent_file_available['fileUrl']
-        prices_file_url = 'http://sberbank.ru' + url_part_of_most_recent_file
-        file_with_prices = requests.get(prices_file_url, allow_redirects=True)
-        return file_with_prices.content
-
-
     if price_file_list:
         file_content = download_gold_bar_prices(price_file_list)
         xls_file_name = 'gold.xls'
@@ -69,44 +104,15 @@ def main():
     else:
         print("File with gold bar prices was not found")
 
-
-    def gold_bar_price_from_xls(xls_file_name):
-        book = xlrd.open_workbook(xls_file_name, encoding_override="cp1252")
-        sheet = book.sheet_by_index(0)
-        return int(sheet.cell_value(11, 3))
-
-
-    def count_price_diff_as_percent(new_price, OLD_PRICE):
-        return int(((new_price - OLD_PRICE) * 100) / OLD_PRICE)
-
-
     OLD_PRICE = 31341
     xls_file_name = 'gold.xls'
     new_price = gold_bar_price_from_xls(xls_file_name)
     price_diff_as_percent = count_price_diff_as_percent(new_price, OLD_PRICE)
     print(textwrap.dedent(f"""
-        {new_price} - 31341 = 
-        {int(new_price - OLD_PRICE)} rub., 
+        {new_price} - 31341 =
+        {int(new_price - OLD_PRICE)} rub.,
         {price_diff_as_percent}%
         """).replace("\n", ""))
-
-
-    def save_new_price_to_make_a_graph():
-        date_list = []
-        with open("gold.csv", "r", encoding="cp1251") as file:
-            reader = csv.DictReader(file, delimiter=",")
-            for line in reader:
-                date_list.append(line['date'])
-
-        if date_list[-1] != current_date_str:
-            data = {'date': current_date_str, 
-                    'old_price': OLD_PRICE,
-                    'new_price': new_price,
-                    'percentage': price_diff_as_percent}
-            with open("gold.csv", "a", encoding="cp1251", newline="") as f:
-                fieldnames = ["date", "old_price", "new_price", "percentage"]
-                writer = csv.DictWriter(f, fieldnames, delimiter=",")
-                writer.writerow(data)
 
 
     def show_price_change_graph():
